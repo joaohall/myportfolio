9b0113401005b844f7ac984471faf2e4de362d09
joaohall <enderfiremite@gmail.com>
2023-09-16 17:10:18 -0300

    Add. navbar responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
+'use client'
+
+import { useEffect, useRef, useState } from 'react';
 
 import Image from "next/image";
 import MouseAnimation from './components/mouseanim'
+import hamburguerIcon from './assets/svg/hamburguerIcon.svg'
 import icon from "./assets/svg/icon.svg";
 import line from "./assets/images/line.png";
 import profilepic from "./assets/images/joaohall.png"
 
 function WelcomeSection(){
   return(
-    <section>
-
+    <section className="h-screen">
+      
     </section>
   )
 }
 function ExperienceSection(){
   return(
-    <section>
+    <section className="h-screen">
 
     </section> 
   )
@@ -22,22 +26,62 @@ function ExperienceSection(){
 
 function NavigationBar(){
   return(
-    <div className="flex flex-row h-screen p-8 gap-8 fixed left-0">
-      <div className="flex flex-col gap-12">
-        <h1>a</h1>
-        <h1>a</h1>
-        <h1>a</h1>
-        <h1>a</h1>
-      </div>
-      <div className="bg-white w-[0.2px] opacity-50 rounded-full "/>
+    <div className="flex justify-center m-auto">
+      <nav className="flex flex-row p-8 gap-8 fixed justify-between items-center w-full max-w-[1440px]">
+        <Image src={icon} width={30} alt='' />
+        <div className="flex flex-row gap-12">
+          <a href='#'>início</a>
+          <a href='#'>Experiência</a>
+          <a href='#'>Blog</a>
+          <a href='#'>Projetos</a>
+        </div>
+      </nav>
     </div>
   )
 }
 
+function NavigationHamburguer(){
+    return(
+     <div className='fixed flex flex-row w-full p-8 justify-between' >
+       <Image src={icon} width={30} alt='' />
+       <button>
+        <Image src={hamburguerIcon} alt='icon' width={30} height={50}/>
+      </button>
+     </div>
+    )
+}
+
 export default function Home(){
+  const [windowSize, setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+  const [isMobile, setIsMobile] = useState(false)
+
+  //Window size getter and navbar hide
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      if(window.innerWidth < 769){
+        setIsMobile(true)
+      }else if(window.innerWidth > 768){
+        setIsMobile(false);
+      }
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
     return(
     <main>
-      <NavigationBar/>
+      {
+        (isMobile) ? <NavigationHamburguer/> : <NavigationBar/>
+      }
       <WelcomeSection/>
       <ExperienceSection/>
     </main>

c045f99dae672683e14ec0bb3808225cdaba1b3e
joaohall <enderfiremite@gmail.com>
2023-09-17 18:18:53 -0300

    Addition of the first section

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,25 @@ import hamburguerIcon from "./assets/svg/hamburguerIcon.svg";
 import icon from "./assets/svg/icon.svg";
 import line from "./assets/images/line.png";
 import profilepic from "./assets/images/joaohall.png";
+import profilewallpaper from "./assets/images/wallpaper.png"
 
 export default function Home() {
   const [pressed, setPressed] = useState(false);
 
   function WelcomeSection() {
-    return <section className="h-screen">
-      
+    return <section className="h-screen p-24 overflow-hidden ">
+        <div className="absolute z-0">
+          <Image src={profilewallpaper} width={100}/>
+        </div>
+      <div className="h-full max-w-[1440px] justify-end m-auto flex flex-col">
+        <div className="z-10">
+          <h1 className="text-6xl font-bold">Eu sou o João Eduardo</h1>
+          <p className="text-2xl mb-7">O seu próximo desenvolvedor </p>
+          <div className="flex gap-10">
+            <button className="p-2 px-6 bg-blue-600 rounded-xl hover:tracking-widest transition-all hover:space-x-9">Ver projetos <span> > </span></button>
+          </div>
+        </div>
+      </div>
     </section>;
   }
   function ExperienceSection() {
@@ -28,11 +40,11 @@ export default function Home() {
       <div className="sm:flex hidden justify-center m-auto">
         <nav className="flex flex-row p-8 gap-8 fixed justify-between items-center w-full max-w-[1440px]">
           <Image src={icon} width={30} alt="" />
-          <div className="flex flex-row gap-12">
-            <a href="#">início</a>
-            <a href="#">Experiência</a>
-            <a href="#">Blog</a>
-            <a href="#">Projetos</a>
+          <div className="flex flex-row gap-12 items-center">
+            <a href="#" className="hover:text-blue-500 transition-all">Início</a>
+            <a href="#" className="hover:text-blue-500 transition-all">Experiência</a>
+            <a href="#" className="hover:text-blue-500 transition-all">Blog</a>
+            <a href="#" className="hover:text-blue-500 transition-all">Projetos</a>
           </div>
         </nav>
       </div>
@@ -41,7 +53,7 @@ export default function Home() {
 
   function NavigationHamburguer() {
     return (
-      <div className="fixed sm:hidden flex flex-row w-full p-8 justify-between z-20 base-animation">
+      <div className="fixed sm:hidden flex flex-row w-full p-8 justify-between z-30 base-animation">
         <Image src={icon} width={30} alt="" />
         <button
           onClick={() => {
@@ -56,15 +68,15 @@ export default function Home() {
   function HamburguerMenu(){
     return(
       <div
-      className={`fixed top-0 left-0 w-full h-full z-10 bg-blue-900 flex items-center p-4 justify-center ${
-        pressed ? 'menu-close' : ' menu-open'
+      className={`fixed top-0 left-0 w-full h-full z-20 bg-blue-900 flex items-center p-4 justify-center ${
+        pressed ? 'menu-open' : ' hidden  menu-close'
       }`}
     >      
       <ul className={`text-3xl font-bold flex flex-col gap-20 text-center `}>
-        <li className={`opacity-0 ${pressed?'text-close':'text-open-1 animation-delay-300'}`}>Início</li>
-        <li className={`opacity-0 ${pressed?'text-close':'text-open-2 animation-delay-400'}`}>Experiência</li>
-        <li className={`opacity-0 ${pressed?'text-close':'text-open-3 animation-delay-500'}`}>Blog</li>
-        <li className={`opacity-0 ${pressed?'text-close':'text-open-4 animation-delay-600'}`}>Projetos</li>
+        <li className={`opacity-0 ${pressed?'text-open-1':'hidden text-close animation-delay-300'}`}>Início</li>
+        <li className={`opacity-0 ${pressed?'text-open-2':'hidden text-close animation-delay-400'}`}>Experiência</li>
+        <li className={`opacity-0 ${pressed?'text-open-3':'hidden text-close animation-delay-500'}`}>Blog</li>
+        <li className={`opacity-0 ${pressed?'text-open-4':'hidden text-close animation-delay-600'}`}>Projetos</li>
       </ul>
     </div>
     )

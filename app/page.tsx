64ffd8e0be6d543373d5ce312da79cb33b55c0eb
joaohall <enderfiremite@gmail.com>
2023-09-18 10:36:56 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -17,18 +17,17 @@ function WelcomeSection() {
           <h1 className="md:text-6xl text-5xl font-bold">Eu sou o João Eduardo</h1>
           <p className="md:text-2xl text-1xl  mb-7">O seu próximo desenvolvedor </p>
           <div className="flex gap-10 md:justify-start justify-center">
-            <button className="p-2 px-6 bg-blue-600 md:text-white rounded-xl hover:tracking-widest transition-all hover:space-x-9 ">
+            <button className="p-2 px-6 bg-[#2400FF] md:text-white rounded-xl hover:tracking-widest transition-all hover:space-x-9 ">
               Ver projetos <span>{seta}</span>
             </button>
           </div>
         </div>
       </div>
-
-      <div className=" absolute h-full w-full top-0 right-0">
+      {/* <div className=" absolute h-full w-full top-0 right-0">
         <div className="bg-gradient-animation h-full w-full">
           <Image src={wallpaper} priority={true} className="-z-30 bg-animation" layout="fill" objectFit="cover" alt="" />
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
@@ -46,13 +45,13 @@ export default function Home() {
     return (
       <div className="z-30">
        <div className="sm:flex hidden justify-center m-auto">
-        <nav className="flex flex-row p-8 gap-8 fixed justify-between items-center w-full max-w-[1440px] z-50 ">
+        <nav className="flex flex-row p-12 gap-8 fixed justify-between items-center w-full max-w-[1440px] z-50 ">
           <Image src={icon} width={30} alt="" className="text-open-1 opacity-0" />
           <div className="flex flex-row gap-12 items-center">
-            <a href="#" className="hover:text-blue-500 transition-opacity opacity-0 text-open-4">Início</a>
-            <a href="#" className="hover:text-blue-500 transition-opacity opacity-0 text-open-3">Experiência</a>
-            <a href="#" className="hover:text-blue-500 transition-opacity opacity-0 text-open-2">Blog</a>
-            <a href="#" className="hover:text-blue-500 transition-opacity opacity-0 text-open-1">Projetos</a>
+            <a href="#" className="hover:text-[#2400FF] transition-all duration-700 opacity-0 text-open-4">Início</a>
+            <a href="#" className="hover:text-[#2400FF] transition-all duration-700 opacity-0 text-open-3">Experiência</a>
+            <a href="#" className="hover:text-[#2400FF] transition-all duration-700 opacity-0 text-open-2">Blog</a>
+            <a href="#" className="hover:text-[#2400FF] transition-all duration-700 opacity-0 text-open-1">Projetos</a>
           </div>
         </nav>
       </div>

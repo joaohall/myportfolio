299a48facce2f4133a88628cee97944affd82575
joaohall <enderfiremite@gmail.com>
2023-11-05 00:46:54 -0300

    .

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,34 +1,41 @@
+'use client'
+import React, { useEffect, useRef } from 'react';
 import Image from "next/image"
-import Projects from "./projectviewer"
+import ProjectsViewer from "./projectviewer"
 
-export default function ProjectsViewer() {
+
+export default function Projects() {
     return (
         <section className="lg:p-12 bg-zinc-900">
             <section className="max-w-[1440px] m-auto">
                 <div className="px-16 py-12">
-                    <p className="text-2xl">Desenvolvimento</p>
+                    <p className="text-2xl text-gray-400">Desenvolvimento</p>
                     <h1 className="text-5xl font-bold">Meus projetos</h1>
                 </div>
                 <div className="flex gap-5 m-10 flex-wrap justify-center">
-                    <Projects
-                        image={'/_DSC0176.jpg'}
+                    <ProjectsViewer
+                        image={'/anatomical.png'}
                         project={'Anatomical'}
                     />
-                    <Projects
-                        image={''}
+                    <ProjectsViewer
+                        image={'/djangoplaceholder.png'}
                         project={'DBooks'}
                     />
-                    <Projects
-                        image={''}
+                    <ProjectsViewer
+                        image={'/djangoplaceholder.png'}
                         project={'Feintel'}
                     />
-                    <Projects
-                        image={''}
+                    <ProjectsViewer
+                        image={'/djangoplaceholder.png'}
                         project={'FlowShop'}
                     />
-                    <Projects
-                        image={''}
+                    <ProjectsViewer
+                        image={'/routini.png'}
                         project={'Routini'}
+                    />
+                    <ProjectsViewer
+                        image={'/infotour.png'}
+                        project={'Infotour'}
                     />
                 </div>
             </section>
--- src/app/components/projectviewer.tsx
+++ src/app/components/projectviewer.tsx
@@ -1,26 +1,58 @@
+'use client'
+
 import Image from "next/image"
+import React, { useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import VanillaTilt from 'vanilla-tilt';
+
+ 
+const options = {
+    reverse:           false,  // reverse the tilt direction
+    max:               10,     // max tilt rotation (degrees)
+    perspective:       1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale:             1.05,      // 2 = 200%, 1.5 = 150%, etc..
+    speed:             300,    // Speed of the enter/exit transition
+    transition:        true,   // Set a transition on enter/exit.
+    axis:              null,   // What axis should be disabled. Can be X or Y.
+    reset:             true,   // If the tilt effect has to be reset on exit.
+    easing:            "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+    glare:             false,   // if it should have a "glare" effect
+    "max-glare":       1,      // the maximum "glare" opacity (1 = 100%, 0.5 = 50%)
+    "glare-prerender": false   // false = VanillaTilt creates the glare elements for you, otherwise
+                               // you need to add .js-tilt-glare>.js-tilt-glare-inner by yourself
+} 
+function Tilt(props:any){  
+    const { options, ...rest } = props;
+    const tilt = useRef(null);
+
+    useEffect(() => {
+        VanillaTilt.init(tilt.current, options);
+      }, [options]);
+
+      return <div ref={tilt} {...rest} />;
+  }
+
 
 export default function ProjectsViewer(props: any) {
     return (
-        <div className="w-96 h-96 rounded-xl relative z-10 border-[1px] flex justify-between bg-slate-500 bg-opacity-20 overflow-hidden border-white/20">
-            <div className="absolute -z-10 h-96 w-96 ">
-                <div className="absolute top-0 bottom-0 right-0 left-0">
-
-                    <Image
-                        src={props.image}
-                        layout="fill"
-                        objectFit="cover"
-                        quality={100}
-                        alt=""
-                        className="opacity-50"
-                        priority
-                    />
+            <Tilt  options={options} className="w-96 h-96 rounded-xl relative z-10 border-[1px] flex justify-between bg-slate-500 bg-opacity-20 overflow-hidden border-white/20">
+                <div className="absolute -z-10 h-96 w-96 ">
+                    <div className="absolute top-0 bottom-0 right-0 left-0">
+                        <Image
+                            src={props.image}
+                            layout="fill"
+                            objectFit="cover"
+                            quality={100}
+                            alt=""
+                            className="opacity-70"
+                            priority
+                        />
+                    </div>
                 </div>
-            </div>
-            <div className="p-6 flex flex-col justify-end">
-                <h1 className="text-2xl font-bold pb-3">{props.project}</h1>
-                <button className="w-fit h-fit p-2 border-[1px] border-white/20 md:text-md text-sm rounded-lg">Ver projeto</button>
-            </div>
-        </div>
+                <div className="p-6 flex flex-col justify-end">
+                    <h1 className="text-2xl font-bold pb-3">{props.project}</h1>
+                    <button className="w-fit h-fit p-2 border-[1px] border-white/20 md:text-md text-sm rounded-lg">Ver projeto</button>
+                </div>
+            </Tilt>
     )
 }
